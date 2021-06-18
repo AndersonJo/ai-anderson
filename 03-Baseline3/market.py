57e1fc1cf3e1a80c933fdc61f0097451f360e8ae
Anderson <a141890@gmail.com>
2021-06-18 18:34:09 +0900

    as

---
+++ 03-Baseline3/market.py
@@ -0,0 +1,16 @@
+import gym as gym
+
+
+class MarketEnv(gym.Env):
+
+    def __init__(self):
+        super().__init__()
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self, mode='human'):
+        pass
